54ec984da0ff27db89c431150904aa966f7378d5
Patryk Kwaśniok <kwasniokpatryk@gmail.com>
2025-08-27 21:53:03 +0200

    feat: add firefox

--- 04-install-desktop.sh
+++ 04-install-desktop.sh
@@ -2,5 +2,8 @@ pacstrap -K /mnt \
 
     # Gnome
     gdm gnome-shell gnome-session gnome-keyring gnome-settings-daemon \
-    gnome-backgrounds gnome-console gnome-control-center gnome-tweaks
+    gnome-backgrounds gnome-console gnome-control-center gnome-tweaks \
+
+    # Extras
+    firefox firefox-developer-edition \
 
